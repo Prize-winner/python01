4703ec14f0dd1b0a9b7af6f1ea4e1cbdb7127d87
Prize-winner <3220016928@qq.com>
2019-06-14 17:34:44 +0800

    2019年6月12日学习的python

---
+++ python01.py
@@ -0,0 +1,18 @@
+#命令
+
+'''
+    sudo vim /etc/hosts
+    输入密码123
+
+    linux 基础命令
+
+    根目录下面有如下：
+    /bin  /etc/  /home
+
+
+    一般在/home 文件夹中创建
+    /sixstar  当前的用户目录
+
+    再往下分
+    /Desktop  /Document /Downloads /Music /Pictures /Public /redis-5.0.0 /templates /videos /示例
+'''
